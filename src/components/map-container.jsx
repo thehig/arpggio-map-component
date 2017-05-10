2b0305a3f80e5653ba9268b86e1bf71a0a6e1d8d
David Higgins <thehig@gmail.com>
2017-05-10 18:31:24 +0100

    Broken linting, but progress

---
+++ src/components/map-container.jsx
@@ -0,0 +1,12 @@
+import React from 'react'; //eslint-disable-line
+
+import styled from 'styled-components';
+
+const MapContainer = styled.div`
+  // background: red;
+  overflow: hidden;
+  border: 1px solid black;
+  padding: 5px;
+`;
+
+export default MapContainer;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+const defaultProperties = {
+  width: 640,
+  height: 480,
+};
+
 ReactDOM.render(
-  <App />,
+  <App {...defaultProperties} />,
   document.getElementById('root'),
 );
