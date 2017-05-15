6edcc62adb11ebd598c5074ddcec117619e9889a
David Higgins <thehig@gmail.com>
2017-05-15 18:08:22 +0100

    Refactor and prepare for controlled-element

--- example/src/example.js
+++ example/src/example.js
@@ -2,14 +2,6 @@ const React = require('react');
 const ReactDOM = require('react-dom');
 const ArpggioMapComponent = require('arpggio-map-component'); //eslint-disable-line
 
-const App = React.createClass({
-  render() {
-    return (
-      <div>
-        <ArpggioMapComponent />
-      </div>
-    );
-  },
-});
+const App = () => (<div> <ArpggioMapComponent /> </div>);
 
 ReactDOM.render(<App />, document.getElementById('app'));
