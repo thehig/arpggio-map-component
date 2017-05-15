ab255168e39500478f3408d23c584c0b49adff60
David Higgins <thehig@gmail.com>
2017-05-15 13:32:44 +0100

    Update as much as possible, ignore reference for linting

--- example/src/example.js
+++ example/src/example.js
@@ -1,6 +1,6 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
-const ArpggioMapComponent = require('arpggio-map-component');
+const ArpggioMapComponent = require('arpggio-map-component'); //eslint-disable-line
 
 const App = React.createClass({
   render() {
