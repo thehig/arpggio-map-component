736e2f9be63e4489ec62f3a804a48cee51646df6
David Higgins <thehig@gmail.com>
2017-05-15 12:45:21 +0100

    Copy in yeoman generated project structure

---
+++ src/ArpggioMapComponent.js
@@ -0,0 +1,9 @@
+var React = require('react');
+
+var ArpggioMapComponent = React.createClass({
+	render () {
+		return <div>arpggio-map-component</div>;
+	}
+});
+
+export default ArpggioMapComponent;
