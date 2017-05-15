6f8d3385b01c8e80e1a8372f887182db04c2f0c2
David Higgins <thehig+github@gmail.com>
2017-05-15 13:33:56 +0100

    Yeoman/restructure (#3)
    * Copy in yeoman generated project structure
    
    * Add back in my previous workings
    
    * Update dependencies
    
    * Reintroduce examples folder and lint some more
    
    * 0.3.0
    
    * Move previous attempt into examples/reference
    
    * Update as much as possible, ignore reference for linting
    
    * 0.3.1

---
+++ src/ArpggioMapComponent.js
@@ -0,0 +1,9 @@
+const React = require('react');
+
+const ArpggioMapComponent = React.createClass({
+  render() {
+    return <div>arpggio-map-component</div>;
+  },
+});
+
+export default ArpggioMapComponent;
